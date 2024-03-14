e1bbc3d515303ed7bf4ee70706973a13a34691ff
Baptiste Haon <163291677+BaptisteHaon@users.noreply.github.com>
2024-03-14 16:10:45 +0100

    Update Functions.R

--- R/Functions.R
+++ R/Functions.R
@@ -26,43 +26,52 @@ OLS_mod <- readRDS("OLS.rds")
 MARS_mod <- readRDS("MARS.rds")
 GBT_mod <- readRDS("GBT.rds")
 
+SF36_matrix$PF <- SF36_matrix$PF/100
+SF36_matrix$RP <- SF36_matrix$RP/100
+SF36_matrix$BP <- SF36_matrix$BP/100
+SF36_matrix$GH <- SF36_matrix$GH/100
+SF36_matrix$VT <- SF36_matrix$VT/100
+SF36_matrix$SF <- SF36_matrix$SF/100
+SF36_matrix$RE <- SF36_matrix$RE/100
+SF36_matrix$MH <- SF36_matrix$MH/100
+
 OLS_mapping <- function (SF36_matrix){
-  stopifnot("PF score must be numeric in the range 0 to 1." = SF36_matrix$PF>=0&SF36_matrix$PF<=1)
-  stopifnot("RP score must be numeric in the range 0 to 1." = SF36_matrix$RP>=0&SF36_matrix$RP<=1)
-  stopifnot("BP score must be numeric in the range 0 to 1." = SF36_matrix$BP>=0&SF36_matrix$BP<=1)
-  stopifnot("GH score must be numeric in the range 0 to 1." = SF36_matrix$GH>=0&SF36_matrix$GH<=1)
-  stopifnot("VT score must be numeric in the range 0 to 1." = SF36_matrix$VT>=0&SF36_matrix$VT<=1)
-  stopifnot("SF score must be numeric in the range 0 to 1." = SF36_matrix$SF>=0&SF36_matrix$SF<=1)
-  stopifnot("RE score must be numeric in the range 0 to 1." = SF36_matrix$RE>=0&SF36_matrix$RE<=1)
-  stopifnot("MH score must be numeric in the range 0 to 1." = SF36_matrix$MH>=0&SF36_matrix$MH<=1)
+  stopifnot("PF score must be numeric in the range 0 to 1." = SF36_matrix$PF>=0&SF36_matrix$PF<=100)
+  stopifnot("RP score must be numeric in the range 0 to 1." = SF36_matrix$RP>=0&SF36_matrix$RP<=100)
+  stopifnot("BP score must be numeric in the range 0 to 1." = SF36_matrix$BP>=0&SF36_matrix$BP<=100)
+  stopifnot("GH score must be numeric in the range 0 to 1." = SF36_matrix$GH>=0&SF36_matrix$GH<=100)
+  stopifnot("VT score must be numeric in the range 0 to 1." = SF36_matrix$VT>=0&SF36_matrix$VT<=100)
+  stopifnot("SF score must be numeric in the range 0 to 1." = SF36_matrix$SF>=0&SF36_matrix$SF<=100)
+  stopifnot("RE score must be numeric in the range 0 to 1." = SF36_matrix$RE>=0&SF36_matrix$RE<=100)
+  stopifnot("MH score must be numeric in the range 0 to 1." = SF36_matrix$MH>=0&SF36_matrix$MH<=100)
   stopifnot("AGE must be numeric in the range 0 to 110." = SF36_matrix$AGE>=0&SF36_matrix$AGE<=100)
   EQ5D_ut_score <- predict(OLS_mod, SF36_matrix)
   return(EQ5D_ut_score)
 }
 
 MARS_mapping <- function (SF36_matrix){
-  stopifnot("PF score must be numeric in the range 0 to 1." = SF36_matrix$PF>=0&SF36_matrix$PF<=1)
-  stopifnot("RP score must be numeric in the range 0 to 1." = SF36_matrix$RP>=0&SF36_matrix$RP<=1)
-  stopifnot("BP score must be numeric in the range 0 to 1." = SF36_matrix$BP>=0&SF36_matrix$BP<=1)
-  stopifnot("GH score must be numeric in the range 0 to 1." = SF36_matrix$GH>=0&SF36_matrix$GH<=1)
-  stopifnot("VT score must be numeric in the range 0 to 1." = SF36_matrix$VT>=0&SF36_matrix$VT<=1)
-  stopifnot("SF score must be numeric in the range 0 to 1." = SF36_matrix$SF>=0&SF36_matrix$SF<=1)
-  stopifnot("RE score must be numeric in the range 0 to 1." = SF36_matrix$RE>=0&SF36_matrix$RE<=1)
-  stopifnot("MH score must be numeric in the range 0 to 1." = SF36_matrix$MH>=0&SF36_matrix$MH<=1)
+  stopifnot("PF score must be numeric in the range 0 to 1." = SF36_matrix$PF>=0&SF36_matrix$PF<=100)
+  stopifnot("RP score must be numeric in the range 0 to 1." = SF36_matrix$RP>=0&SF36_matrix$RP<=100)
+  stopifnot("BP score must be numeric in the range 0 to 1." = SF36_matrix$BP>=0&SF36_matrix$BP<=100)
+  stopifnot("GH score must be numeric in the range 0 to 1." = SF36_matrix$GH>=0&SF36_matrix$GH<=100)
+  stopifnot("VT score must be numeric in the range 0 to 1." = SF36_matrix$VT>=0&SF36_matrix$VT<=100)
+  stopifnot("SF score must be numeric in the range 0 to 1." = SF36_matrix$SF>=0&SF36_matrix$SF<=100)
+  stopifnot("RE score must be numeric in the range 0 to 1." = SF36_matrix$RE>=0&SF36_matrix$RE<=100)
+  stopifnot("MH score must be numeric in the range 0 to 1." = SF36_matrix$MH>=0&SF36_matrix$MH<=100)
   stopifnot("AGE must be numeric in the range 0 to 110." = SF36_matrix$AGE>=0&SF36_matrix$AGE<=100)
   EQ5D_ut_score <- predict(MARS_mod, SF36_matrix)
   return(EQ5D_ut_score)
 }
 
 GBT_mapping <- function (SF36_matrix){
-  stopifnot("PF score must be numeric in the range 0 to 1." = SF36_matrix$PF>=0&SF36_matrix$PF<=1)
-  stopifnot("RP score must be numeric in the range 0 to 1." = SF36_matrix$RP>=0&SF36_matrix$RP<=1)
-  stopifnot("BP score must be numeric in the range 0 to 1." = SF36_matrix$BP>=0&SF36_matrix$BP<=1)
-  stopifnot("GH score must be numeric in the range 0 to 1." = SF36_matrix$GH>=0&SF36_matrix$GH<=1)
-  stopifnot("VT score must be numeric in the range 0 to 1." = SF36_matrix$VT>=0&SF36_matrix$VT<=1)
-  stopifnot("SF score must be numeric in the range 0 to 1." = SF36_matrix$SF>=0&SF36_matrix$SF<=1)
-  stopifnot("RE score must be numeric in the range 0 to 1." = SF36_matrix$RE>=0&SF36_matrix$RE<=1)
-  stopifnot("MH score must be numeric in the range 0 to 1." = SF36_matrix$MH>=0&SF36_matrix$MH<=1)
+  stopifnot("PF score must be numeric in the range 0 to 1." = SF36_matrix$PF>=0&SF36_matrix$PF<=100)
+  stopifnot("RP score must be numeric in the range 0 to 1." = SF36_matrix$RP>=0&SF36_matrix$RP<=100)
+  stopifnot("BP score must be numeric in the range 0 to 1." = SF36_matrix$BP>=0&SF36_matrix$BP<=100)
+  stopifnot("GH score must be numeric in the range 0 to 1." = SF36_matrix$GH>=0&SF36_matrix$GH<=100)
+  stopifnot("VT score must be numeric in the range 0 to 1." = SF36_matrix$VT>=0&SF36_matrix$VT<=100)
+  stopifnot("SF score must be numeric in the range 0 to 1." = SF36_matrix$SF>=0&SF36_matrix$SF<=100)
+  stopifnot("RE score must be numeric in the range 0 to 1." = SF36_matrix$RE>=0&SF36_matrix$RE<=100)
+  stopifnot("MH score must be numeric in the range 0 to 1." = SF36_matrix$MH>=0&SF36_matrix$MH<=100)
   stopifnot("AGE must be numeric in the range 0 to 110." = SF36_matrix$AGE>=0&SF36_matrix$AGE<=100)
   EQ5D_ut_score <- predict(GBT_mod, SF36_matrix)
   return(EQ5D_ut_score)
